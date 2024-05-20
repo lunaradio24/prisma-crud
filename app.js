78912ffa7ea6da2da94934c4c2070a8c106c93cc
Changjun Yeo <lunaradio24@gmail.com>
2024-05-20 18:24:14 +0900

    add: prettier

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-import express from "express";
-import PostsRouter from "./routes/posts.router.js";
+import express from 'express';
+import PostsRouter from './routes/posts.router.js';
 
 const app = express();
 const PORT = 3017;
 
 app.use(express.json());
-app.use("/api", [PostsRouter]);
+app.use('/api', [PostsRouter]);
 
 app.listen(PORT, () => {
-  console.log(PORT, "포트로 서버가 열렸어요!");
+  console.log(PORT, '포트로 서버가 열렸어요!');
 });
